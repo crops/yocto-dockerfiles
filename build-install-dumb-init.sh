63bd856e670a1ac8e1329f3083300d6e99d3f917
Randy Witt <randy.e.witt@intel.com>
2020-06-24 12:18:27 -0700

    build-install-dumb-init.sh: Fix intermittent test failure
    Use an upstream patch to increase the timeout in the dumb-init test
    code. The sleep wasn't long enough for slower systems(such as
    virtualized ci). This would cause the test to intermittently fail.
    
    Signed-off-by: Randy Witt <randy.e.witt@intel.com>

--- build-install-dumb-init.sh
+++ build-install-dumb-init.sh
@@ -21,8 +21,15 @@ fi
 
 wget https://github.com/Yelp/dumb-init/archive/v1.2.0.tar.gz || exit 1
 echo "74486997321bd939cad2ee6af030f481d39751bc9aa0ece84ed55f864e309a3f  v1.2.0.tar.gz" > sha256sums || exit 1
+
+wget https://github.com/Yelp/dumb-init/commit/0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch || exit 1
+echo "eeabd5b66cd2fae2d2edb38f47b3325c56138521f1ea3381042ab156b17785cc  0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch" > sha256sums || exit 1
+
 sha256sum -c sha256sums || exit 1
 tar xf v1.2.0.tar.gz || exit 1
+
+# patch to increase sleep in a test that can fail on slower systems
+patch -d dumb-init-1.2.0 -p1 < 0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch
 
 # Replace the versions of python used for testing dumb-init. Since it is
 # testing c code, and not python it shouldn't matter. Also remove the
