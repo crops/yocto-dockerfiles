11d49e066f5a421f105a091cf1c8a8773bf30ff5
Randy Witt <randy.e.witt@intel.com>
2020-06-25 16:38:06 -0700

    buildtools: Pick the appropriate one based on architecture
    Since arm64 images may now be built, we need to pick the appropriate
    architecture when downloading buildtools.
    
    Signed-off-by: Randy Witt <randy.e.witt@intel.com>

--- install-buildtools.sh
+++ install-buildtools.sh
@@ -2,11 +2,23 @@
 
 set -e
 
-wget https://downloads.yoctoproject.org/releases/yocto/yocto-3.1/buildtools/x86_64-buildtools-extended-nativesdk-standalone-3.1.sh
+if [ "$(uname -m)" = "aarch64" ]; then
+    BUILDTOOLS="aarch64-buildtools-extended-nativesdk-standalone-3.1.3.sh"
+    SHA256SUM="ea342902dafb53f6f7b7df948263a086b116cb0944051e930a8c1edae3ea3e0d"
+elif [ "$(uname -m)" = "x86_64" ]; then
+    BUILDTOOLS="x86_64-buildtools-extended-nativesdk-standalone-3.1.3.sh"
+    SHA256SUM="809de42e33b86d964621752e914883077aeef7da4fe8c7f188669e222e739256"
+else
+    echo "Unsupported architecture, can't install buildtools."
+    exit 1
+fi
 
-echo "4fd00aee6a1e8d85920db8309ea10acb29cbfe9f411a4b127597aabf2013afd4  x86_64-buildtools-extended-nativesdk-standalone-3.1.sh" > SHA256SUMS
+
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-3.1.3/buildtools/${BUILDTOOLS}
+
+echo "${SHA256SUM} ${BUILDTOOLS}" > SHA256SUMS
 sha256sum -c SHA256SUMS
 rm SHA256SUMS
 
-bash x86_64-buildtools-extended-nativesdk-standalone-3.1.sh -y
-rm x86_64-buildtools-extended-nativesdk-standalone-3.1.sh
+bash ${BUILDTOOLS} -y
+rm ${BUILDTOOLS}
