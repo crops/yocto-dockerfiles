09b2576da84fda96571982c84d1426cd088293f3
Randy Witt <randy.e.witt@intel.com>
2021-04-22 17:58:23 -0700

    build-install-dumb-init: Don't overwite dumb-init sha256sum
    Make sure that the sha256sum is verified for the v1.20.tar.gz file.
    
    The sha256sum to be checked for the downloaded v1.20.tar.gz was being
    overwritten in the sha256sums file. This means that the sha256sum was
    not being verified.
    
    Signed-off-by: Randy Witt <randy.e.witt@intel.com>

--- build-install-dumb-init.sh
+++ build-install-dumb-init.sh
@@ -23,7 +23,7 @@ wget https://github.com/Yelp/dumb-init/archive/v1.2.0.tar.gz || exit 1
 echo "74486997321bd939cad2ee6af030f481d39751bc9aa0ece84ed55f864e309a3f  v1.2.0.tar.gz" > sha256sums || exit 1
 
 wget https://github.com/Yelp/dumb-init/commit/0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch || exit 1
-echo "eeabd5b66cd2fae2d2edb38f47b3325c56138521f1ea3381042ab156b17785cc  0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch" > sha256sums || exit 1
+echo "eeabd5b66cd2fae2d2edb38f47b3325c56138521f1ea3381042ab156b17785cc  0708f2779cbcd10e08c6e4cb6551ccea93d75f09.patch" >> sha256sums || exit 1
 
 sha256sum -c sha256sums || exit 1
 tar xf v1.2.0.tar.gz || exit 1
