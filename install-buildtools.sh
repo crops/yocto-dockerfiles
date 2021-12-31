56dfe0121cc65f92a7c3a0618a970ff7b0b23aa7
Tim Orling <tim.orling@konsulko.com>
2021-12-31 11:03:59 -0800

    install-buildtools.sh: bump to 3.1.13 release
    Pick up the security and bug fixes from the latest 'dunfell' LTS release
    buildtools-extended-tarball.
    
    Also bump 3.1.3 to 3.1.13 in distro-entry.sh
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- install-buildtools.sh
+++ install-buildtools.sh
@@ -9,18 +9,18 @@
 set -e
 
 if [ "$(uname -m)" = "aarch64" ]; then
-    BUILDTOOLS="aarch64-buildtools-extended-nativesdk-standalone-3.1.3.sh"
-    SHA256SUM="ea342902dafb53f6f7b7df948263a086b116cb0944051e930a8c1edae3ea3e0d"
+    BUILDTOOLS="aarch64-buildtools-extended-nativesdk-standalone-3.1.13.sh"
+    SHA256SUM="de63845b8d7d3bbd49ea96cd94be3df7ca6e935d0ab510d30c00fa35e3e5e6cd"
 elif [ "$(uname -m)" = "x86_64" ]; then
-    BUILDTOOLS="x86_64-buildtools-extended-nativesdk-standalone-3.1.3.sh"
-    SHA256SUM="809de42e33b86d964621752e914883077aeef7da4fe8c7f188669e222e739256"
+    BUILDTOOLS="x86_64-buildtools-extended-nativesdk-standalone-3.1.13.sh"
+    SHA256SUM="3f6a5e150de674d8098223ae1cfa26051b692d2ed04ce00ef247a836c36a0c41"
 else
     echo "Unsupported architecture, can't install buildtools."
     exit 1
 fi
 
 
-wget https://downloads.yoctoproject.org/releases/yocto/yocto-3.1.3/buildtools/${BUILDTOOLS}
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-3.1.13/buildtools/${BUILDTOOLS}
 
 echo "${SHA256SUM} ${BUILDTOOLS}" > SHA256SUMS
 sha256sum -c SHA256SUMS
