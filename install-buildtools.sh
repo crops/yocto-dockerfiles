36d6fdb42f48106612b81076da1a78d84fe3a57b
Eilís 'pidge' Ní Fhlannagáin <pidge@baylibre.com>
2023-01-12 15:06:31 +0000

    install-buildtools.sh: Update to newest release.
    This obviously hasn't been touched in a bit. I've made it look
    closer to install-buildtools-make.sh but honestly, the two scripts
    should just be merged.
    
    Signed-off-by: Eilís 'pidge' Ní Fhlannagáin <pidge@baylibre.com>

--- install-buildtools.sh
+++ install-buildtools.sh
@@ -8,19 +8,19 @@
 #
 set -e
 
+RELEASE="4.1"
 if [ "$(uname -m)" = "aarch64" ]; then
-    BUILDTOOLS="aarch64-buildtools-extended-nativesdk-standalone-3.1.13.sh"
-    SHA256SUM="de63845b8d7d3bbd49ea96cd94be3df7ca6e935d0ab510d30c00fa35e3e5e6cd"
+    BUILDTOOLS="aarch64-buildtools-extended-nativesdk-standalone-${RELEASE}.sh"
+    SHA256SUM="d5c0dd3e43c62f0465a9335f450d9f5f6861e4b3d39b0f04174bd50e6861c96e"
 elif [ "$(uname -m)" = "x86_64" ]; then
-    BUILDTOOLS="x86_64-buildtools-extended-nativesdk-standalone-3.1.13.sh"
-    SHA256SUM="3f6a5e150de674d8098223ae1cfa26051b692d2ed04ce00ef247a836c36a0c41"
+    BUILDTOOLS="x86_64-buildtools-extended-nativesdk-standalone-${RELEASE}.sh"
+    SHA256SUM="d360ac01016c848f713d6dd7848f25d0a5319e96e2dd279ab37ffcbd7320dbbe"
 else
-    echo "Unsupported architecture, can't install buildtools."
+    echo "Unsupported architecture, can't install buildtools-make."
     exit 1
 fi
 
-
-wget https://downloads.yoctoproject.org/releases/yocto/yocto-3.1.13/buildtools/${BUILDTOOLS}
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-${RELEASE}/buildtools/${BUILDTOOLS}
 
 echo "${SHA256SUM} ${BUILDTOOLS}" > SHA256SUMS
 sha256sum -c SHA256SUMS
