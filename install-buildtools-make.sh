a7048d674024caadf47c4333ff41e57662e3156d
Eilís 'pidge' Ní Fhlannagáin <pidge@baylibre.com>
2023-01-12 15:01:41 +0000

    install-buildtools-make.sh: Fix missing *buildtools-make-nativesdk-standalone-4.1.sh
    *buildtools-make-nativesdk-standalone-4.1.sh directory is missing on the
    autobuilder. Instead of finding it again, just bump release to 4.1 where
    we can find them.
    
    Signed-off-by: Eilís 'pidge' Ní Fhlannagáin <pidge@baylibre.com>

--- install-buildtools-make.sh
+++ install-buildtools-make.sh
@@ -9,21 +9,19 @@
 #
 set -e
 
-RELEASE="4.0"
+RELEASE="4.1"
 if [ "$(uname -m)" = "aarch64" ]; then
     BUILDTOOLS="aarch64-buildtools-make-nativesdk-standalone-${RELEASE}.sh"
-    SHA256SUM="a6b1cd89b7b5d8d7ea540c2d6c101ef84fd8c78a125e407b6b3ad8427f5a64f3"
+    SHA256SUM="ed241869743ac795d1a988246953df5931f68c22108d6f86ebc485b773d28db4"
 elif [ "$(uname -m)" = "x86_64" ]; then
     BUILDTOOLS="x86_64-buildtools-make-nativesdk-standalone-${RELEASE}.sh"
-    SHA256SUM="79ce0518e1b60cb854d3701350179f471a2a781ae695045ff13a62995923dbce"
+    SHA256SUM="d9cc8a4f76392e23f9b2854af78d460e99bb5e4cbb82de6ccca0f6be7506f652"
 else
     echo "Unsupported architecture, can't install buildtools-make."
     exit 1
 fi
 
-# FIXME: temporarily use pre-release URL
-wget https://autobuilder.yocto.io/pub/non-release/20220413-28/buildtools/${BUILDTOOLS}
-# wget https://downloads.yoctoproject.org/releases/yocto/yocto-${RELEASE}/buildtools/${BUILDTOOLS}
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-${RELEASE}/buildtools/${BUILDTOOLS}
 
 echo "${SHA256SUM} ${BUILDTOOLS}" > SHA256SUMS
 sha256sum -c SHA256SUMS
